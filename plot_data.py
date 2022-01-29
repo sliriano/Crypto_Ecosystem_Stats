b687adf1b79892e7f96a0d3ba1eb2443f6c56a20
jdbabbitt <jdbabbitt@yahoo.com>
2022-01-29 17:31:38 -0500

    update plots

--- plot_data.py
+++ plot_data.py
@@ -1,6 +1,9 @@
 from dbhelper import DBHelper
 import matplotlib.dates
 import matplotlib.pyplot as plt
+import plotly.offline as py
+import plotly.express as px
+import plotly.graph_objects as go
 
 def plot_marketcap(id):
     db = DBHelper()
@@ -16,11 +19,31 @@ def plot_marketcap(id):
         mc.append(i[2])
         dates.append(i[4])
 
+    fig = go.Figure(go.Scatter(
+        x = dates,
+        y = mc
+    ))
+
+    fig.update_layout(
+        xaxis_title = "Dates (UTC)\n(Month/Day/Hour)",
+        yaxis_title = "Market Cap (Billions USD)",
+        title = data[0][1] + " Market Capitalization Over Time", title_x=0.5)
+
+    fig.update_xaxes(tickangle=45,
+                    tickmode = 'array',
+                    tickvals = dates[0::5])
+
+    py.plot(fig)
+
+
+
     plt.plot(dates,mc)
     plt.ylabel("Market Cap (Hundreds of Billions)")
     plt.xlabel("Dates (UTC)\n(Month/Day/Hour)")
     plt.title(data[0][1] + " Market Capitalization Over Time")
-    plt.show()
+    #plt.show()
+
+
 
 def plot_volume(id):
     db = DBHelper()
